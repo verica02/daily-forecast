41788e3012a92241ae4dd417f20c794c1ebd9cec
verica02 <vspaskovska@gmail.com>
2021-11-30 15:01:41 +0100

    adding the weather forecast

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 require('dotenv').config();
+const fetch = require('node-fetch');
 const nodemailer = require("nodemailer");
+
 
 (async function run(){
 console.log("running forecast");
+
+const locationRequest = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/US/search?q=${encodeURIComponent('Philadelphia')}&apikey=${process.env.ACCUWEATHER_API_KEY}`);
+const locationData = await locationRequest.json();
+const locationKey = locationData[0].Key;
+
+const forecastRequest = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?apikey=${process.env.ACCUWEATHER_API_KEY}`);
+const forecastData = await forecastRequest.json();
+
+const temperature = forecastData.DailyForecasts[0].Temperature;
+
+
+
 
 let transporter = nodemailer.createTransport({
 	host: "smtp.gmail.com",
@@ -26,6 +40,10 @@ let transporter = nodemailer.createTransport({
 	html: `
 	
 	<h1>Daily Forecast</h1>
+	<h2>Weather</h2>
+	<p>Forecast: ${forecastData.Headline.Text}</p>
+	<p>Min: ${temperature.Minimum.Value}° ${temperature.Minimum.Unit} </p>
+	<p>Max: ${temperature.Maximum.Value}° ${temperature.Maximum.Unit} </p>
 	
 	`, // html body
       });
